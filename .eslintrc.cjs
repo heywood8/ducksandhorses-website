d57ba0503c31188739e3cbca2f2a5779b42ac49a
heywood8 <lopatinikita@gmail.com>
2025-09-19 14:44:21 +0400

    phase3

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   root: true,
   env: { es2022: true, browser: true, node: true },
   parser: '@typescript-eslint/parser',
-  parserOptions: { project: null, ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['@typescript-eslint', 'import', 'astro'],
   extends: [
     'eslint:recommended',
@@ -11,21 +11,20 @@ module.exports = {
     'plugin:astro/recommended',
     'plugin:import/recommended'
   ],
-  settings: {
-    'import/resolver': {
-      typescript: {},
-    }
-  },
+  settings: {},
   rules: {
-    'import/order': ['warn', { 'newlines-between': 'always', groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'], alphabetize: { order: 'asc', caseInsensitive: true } }],
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+  'import/order': ['warn', { 'newlines-between': 'always', groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'], alphabetize: { order: 'asc', caseInsensitive: true }, warnOnUnassignedImports: true }],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'error'
   },
   overrides: [
     {
       files: ['**/*.astro'],
       parser: 'astro-eslint-parser',
       parserOptions: { parser: '@typescript-eslint/parser', extraFileExtensions: ['.astro'] },
-      rules: {}
+      rules: {
+        'import/no-unresolved': 'off'
+      }
     }
   ]
 };
---
+++ src/pages/sitemap.xml.js
@@ -0,0 +1,31 @@
+import { siteConfig } from '../lib/siteConfig';
+
+// Basic sitemap (Phase 3). Later we can auto-discover content collections.
+export async function get() {
+  const base = siteConfig.url.replace(/\/$/, '');
+  const staticPaths = [
+    '/',
+    '/about/',
+    '/services/',
+    '/services/individual/',
+    '/services/couples/',
+    '/services/telehealth/',
+    '/services/modalities/',
+    '/conditions/',
+    '/conditions/anxiety/',
+    '/conditions/depression/',
+    '/conditions/burnout/',
+    '/faq/',
+    '/contact/',
+    '/privacy/',
+    '/emergency/'
+  ];
+  const urls = staticPaths.map(p => `<url><loc>${base}${p}</loc></url>`).join('');
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`;
+  return new Response(xml, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/xml'
+    }
+  });
+}
